9c2b9551669968d6c0bee5087dc46ea585821371
Tom1sk <tom.ruzicka21@gmail.com>
2023-03-25 16:07:42 +0100

    TestRepeating

--- src/Conditions.java
+++ src/Conditions.java
@@ -1,8 +1,6 @@
 import java.util.Scanner;
 public class Conditions {
         public static void main(String[] args) {
-
-
             Scanner sc = new Scanner(System.in);
             System.out.println("Zadej nějaké číslo, ze kterého spočítám odmocninu: ");
             int a = Integer.parseInt(sc.nextLine());
